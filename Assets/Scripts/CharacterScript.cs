c832e5efa1121bcc153cc958bfe27813dc7cd9dd
SulaimanRahman <60456416+SulaimanRahman@users.noreply.github.com>
2021-04-19 17:27:30 -0700

    Added the model and added some animation

---
+++ Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Windows.Input;
+
+public class CharacterScript : MonoBehaviour
+{
+    DataSingleton dataSingleton;
+    Animator constrainerAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dataSingleton = DataSingleton.Instance;
+        constrainerAnimator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dataSingleton.getPersonCamera() == 0) // first person camera
+        {
+            if (Input.GetKey(KeyCode.C)) //switch to second person
+            {
+                dataSingleton.setPersonCamera(1);
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                if (Input.GetKey(KeyCode.W))
+                {
+                    constrainerAnimator.runtimeAnimatorController = Resources.Load("Character Anim/Running") as RuntimeAnimatorController;
+                    constrainerAnimator.Play("Entry");
+                }
+            } else
+            {
+                if (Input.GetKey(KeyCode.W))
+                {
+                    constrainerAnimator.runtimeAnimatorController = Resources.Load("Character Anim/Walking") as RuntimeAnimatorController;
+                    constrainerAnimator.Play("Entry");
+                }
+            }
+            
+
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                if (dataSingleton.getHoldingWeapon() == false)
+                {
+                    constrainerAnimator.runtimeAnimatorController = Resources.Load("Character Anim/NoWeaponIdle") as RuntimeAnimatorController;
+                    constrainerAnimator.Play("Entry");
+                } 
+                else
+                {
+                    constrainerAnimator.runtimeAnimatorController = Resources.Load("Character Anim/WeaponIdle") as RuntimeAnimatorController;
+                    constrainerAnimator.Play("Entry");
+                }
+            }
+        } 
+        else if (dataSingleton.getPersonCamera() == 1) // second person camera
+        {
+            if (Input.GetKey(KeyCode.C)) //switch to first person
+            {
+                dataSingleton.setPersonCamera(0);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/DataSingleton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataSingleton : MonoBehaviour
+{
+
+    private DataSingleton()
+    {
+    }
+
+    private static DataSingleton instance = null;
+
+    private int personCamera = 0; //0 -> first person, 1 -> third Person
+    private bool holdingWeapon = true;
+
+
+    public static DataSingleton Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new DataSingleton();
+            }
+
+            return instance;
+        }
+    }
+
+    public void setHoldingWeapon(bool status)
+    {
+        holdingWeapon = status;
+    }
+
+    public void setPersonCamera (int value)
+    {
+        personCamera = value;
+    }
+
+    public int getPersonCamera()
+    {
+        return personCamera;
+    }
+
+    public bool getHoldingWeapon()
+    {
+        return holdingWeapon;
+    }
+}
