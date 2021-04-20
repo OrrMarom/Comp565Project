ac19164c38139f7a25c03f9a87d8a0dbfce68906
Brandon Dahl <brandon.dahl77@gmail.com>
2021-04-19 20:19:12 -0700

    update from main

---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public float speed;
+    public float bulletSpeed;
+    Rigidbody rb;
+
+    GameObject prefabBullet;
+
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        prefabBullet = Resources.Load("Bullet") as GameObject;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Disabled bullets to practice cubes with raycast
+
+        /*
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(prefabBullet) as GameObject;
+            GameObject hiddenBullet = GameObject.Find("hiddenBullet");
+            //float x = transform.position.x;
+            //float y = transform.position.y;
+            //float z = transform.position.z - 2f;
+            float x = hiddenBullet.transform.position.x;
+            float y = hiddenBullet.transform.position.y;
+            float z = hiddenBullet.transform.position.z;
+
+            //            bullet.transform.position = transform.position + transform.forward * (-2f);
+
+            bullet.transform.rotation = hiddenBullet.transform.rotation;
+
+            bullet.transform.position = new Vector3(x, y, z);
+
+            Rigidbody rigidbody = bullet.GetComponent<Rigidbody>();
+            rigidbody.velocity = (-1) * transform.forward * bulletSpeed;
+            audioSource.Play();
+
+            // Add bullet collision
+            bullet.AddComponent<DestroyCubes>();
+        }
+        */
+    }
+}
---
+++ Assets/Scripts/Level/SkyManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyManager : MonoBehaviour
+{
+    GameObject skydome;
+    GameObject clouds;
+    GameObject stars;
+    float offsetIncrement = 0.01f;
+    float textureUVOffset = 0.0f;
+
+    Vector3 cloudPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skydome = GameObject.Find("SkyDome");
+        clouds = GameObject.Find("Clouds");
+        stars = GameObject.Find("SkyDome/Stars");
+        cloudPosition = clouds.transform.position;
+        cloudPosition = new Vector3(34.0f, 24.0f, 24.0f);
+    }
+
+    void Update()
+    {
+        changeCloudPosition(0.0f * Time.deltaTime, 1.0f * Time.deltaTime);
+    }
+
+
+    void changeTimeOfDay(float offset) {
+        if (offset <= 1 || offset >= 0)
+        {
+            skydome.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+        }
+    }
+
+    void changeCloudPosition(float x, float z) {
+        Vector3 offset = new Vector3(x, 0, z);
+        clouds.transform.position += offset;
+        if (clouds.transform.position.x > 500.0f || clouds.transform.position.z > 500.0f) {
+            clouds.transform.position = new Vector3(34.0f, 24.0f, -500.0f);
+        }
+    }
+
+    void changeStarsPosition(Vector3 offset) {
+        stars.transform.Translate(offset);
+    }
+}
