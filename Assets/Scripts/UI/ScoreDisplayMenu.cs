a63eb8239c1262d083d35cc5fe92c2f0e0a82c7b
OrrMarom <67430740g+OrrMarom@users.noreply.github.com>
2021-05-17 10:14:03 -0700

    Added Scores

---
+++ Assets/Scripts/UI/ScoreDisplayMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using TMPro;
+
+
+public class ScoreDisplayMenu : MonoBehaviour
+{
+
+    private TextMeshProUGUI textMesh;
+    private int score;
+    private string saveFile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        saveFile = Application.persistentDataPath + "/Comp565_Group4_FinalProject.json";
+        textMesh=GetComponent<TextMeshProUGUI>();
+        string Scores = readFile();
+        Scores = Scores.Replace(",","\n");
+        textMesh.text= Scores;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string readFile()
+    {
+        // Does the file exist?
+        if (File.Exists(saveFile))
+        {
+            // Read the entire file and save its contents.
+            return File.ReadAllText(saveFile);
+        }
+        return "";
+    }
+}
