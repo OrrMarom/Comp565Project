c363826540867fbb684aab85d36f2241b2b0e961
OrrMarom <67430740g+OrrMarom@users.noreply.github.com>
2021-05-17 15:25:49 -0700

    tiny camera

--- Assets/Scripts/Hud/HealthBar.cs
+++ Assets/Scripts/Hud/HealthBar.cs
@@ -6,13 +6,13 @@ public class HealthBar : MonoBehaviour
 {
     public Image Health;
     private float fillAmount;
-    private float fullHealth = 1.0f;
+    private float fullHealth = .75f;
     //private int maxHealthCount = 500; // 500 health.
 
     // Start is called before the first frame update
     void Start()
     {
-       Health.fillAmount = fullHealth;
+       Health.fillAmount = 0.5f;
     }
 
     // public updateHealth(int newHealth) {
