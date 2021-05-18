dbb6b9d3fa7f3094980fc34007ce6f0f1b8ba737
OrrMarom <67430740g+OrrMarom@users.noreply.github.com>
2021-05-17 19:02:30 -0700

    final submission
    cleared some extra scenes we dont use

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         //SceneManager.LoadScene("Level1");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("MazeLevel");
     }
     public void LoadMenu()
     {
